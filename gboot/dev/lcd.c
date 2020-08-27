af944b9a0b5eb12bc6b5fbdf6fd90e52a161b71a
Derek Lu <dereklu76@gmail.com>
2020-08-27 13:52:10 +0800

    Add some code for lcd display.

---
+++ gboot/dev/lcd.c
@@ -0,0 +1,75 @@
+#include "lcd.h"
+
+unsigned short lcd_buf[240][320] = {0};
+
+void Lcd_Init(void)
+{
+	/*Initialize the IO Pin to be lcd.*/
+	GPCCON = 0xaaaaaaaa;
+	GPDCON = 0xaaaaaaaa;
+	GPGCON |= (3<<8);
+
+	/*VCLK= , 16bpp TFT, Disable LCD output*/
+	LCDCON1 = LCDCON1_VAL;
+
+	/*Set VSPW, VBPD, LINEVAL, VFPD*/
+	LCDCON2 = LCDCON2_VAL;
+
+	/*Set HBPD, HOZVAL, HFPD*/
+	LCDCON3 = LCDCON3_VAL;
+
+	/*Set HSPW*/
+	LCDCON4 = LCDCON4_VAL;
+
+	LCDCON5 = LCDCON5_VAL;	
+
+	/*start address of lcd buffer*/
+	LCDSADDR1 = (((unsigned long)lcd_buf)>>1);
+
+	/*end address of lcd buffer*/
+	LCDSADDR2 = (LCDSADDR1&0x1fffff)+320*240;
+
+	LCDSADDR3 = ((0<<11)|(320<<0));
+
+	/*Enable LCD_PWREN output signal*/
+	LCDCON5 |= (1<<LCDCON5_PWREN_POS);
+
+	/*Enable video output*/
+	LCDCON1 |= (1<<LCDCON1_ENVID_POS);
+}
+
+
+void Lcd_Delay(void)
+{
+	int i;
+
+	for(i = 0; i < 0xffffff; i++)
+	{
+	
+	}
+}
+
+
+void Lcd_Test(void)
+{
+	int i, j;
+
+	for(i = 0; i < 240; i++)
+	{
+		for(j = 0; j < 320; j++)
+		{
+			lcd_buf[i][j] = ((0x00<<11)|(0x00<<5)|(0x1f<<0));
+			Lcd_Delay();
+		}
+	}
+
+	for(i = 0; i < 240; i++)
+	{
+		for(j = 0; j < 320; j++)
+		{
+			lcd_buf[i][j] = ((0x00<<11)|(0x3f<<5)|(0x00<<0));
+		}
+	}
+}
+
+
--- gboot/main.c
+++ gboot/main.c
@@ -4,7 +4,7 @@
 #include "nandflash.h"
 #include "uart.h"
 #include "printf.h"
-
+#include "lcd.h"
 
 void gboot_main(void)
 {
@@ -22,40 +22,11 @@ void gboot_main(void)
 
 	Uart0_Init();
 
-	ret = NF_BlockErase(0);
-	if(ret)
-	{
-		printf("NF_BlockErase Error!\r\n");
-	}
-
-	ret = 0;
-	for(i = 0; i < 2048; i++)
-	{
-		buf[i] = ret++;
-	}
-	ret = NF_PageWrite(0, buf);
-	if(ret)
-	{
-		printf("NF_PageWrite Error!\r\n");
-	}
-
-	for(i = 0; i < 2048; i++)
-	{
-		buf[i] = 0;
-	}
-	NF_PageRead(0, buf);
-
-	printf("NFCONF=%x\r\n", NFCONF);
-	printf("NFCONT=%x\r\n", NFCONT);
-	printf("NFSTAT=%x\r\n", NFSTAT);
-
-	for(i = 0; i < 2048; i++)
-	{
-		printf("buf[%d]=%x\r\n", i, buf[i]);
-	}
+	Lcd_Init();
 
 	while(1)
 	{
+		Lcd_Test();
 	}
 }
 
